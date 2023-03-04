38dd82a2c08f3c202454ae06e762610ae656a8a2
Kaiwen Li <likw18@mails.tsinghua.edu.cn>
2023-03-04 15:25:36 +0800

    tutor-helper: fixed some bugs (listed below)
      1. 特殊课程标记.1可能不存在
      2. 特殊课程标记新增“交换生课程”类型
      3. 由于原先策略为预处理后读取所在文件夹内的所有xlsx表格，可能出现多读，
         进行修复并顺便补充可以输入xls文件list或文件夹list的功能

--- gpa/calculator/loader.py
+++ gpa/calculator/loader.py
@@ -6,19 +6,25 @@ import pandas as pd
 from gpa.calculator.models import Student, Course, Grade
 from gpa.calculator.storage import Storage
 
+import logging
+
 
 class Loader:
-    def __init__(self, path):
-        if os.path.isdir(path):
-            data_files = glob.glob(os.path.join(path, '*'))
-            self.data_files = [f for f in data_files if f[-5:] == ".xlsx"]
-        elif os.path.isfile(path) and path[-5:] == ".xlsx":
-            self.data_files = [path]
-        else:
-            raise TypeError("Path {} should be a valid file or directory.".format(path))
+    def __init__(self, paths):
+        if type(paths) is not list:
+            paths = [paths]
+        for path in paths:
+            if os.path.isdir(path):
+                data_files = glob.glob(os.path.join(path, '*'))
+                self.data_files = [f for f in data_files if f[-5:] == ".xlsx"]
+            elif os.path.isfile(path) and path[-5:] == ".xlsx":
+                self.data_files = [path]
+            else:
+                logging.error("Path {} should be a valid file or directory, ignored.".format(path))
 
         if len(self.data_files) == 0:
-            raise ValueError("Path {} does not specify a valid data source file.".format(path))
+            logging.fatal("Path {} does not specify any valid data source file.".format(paths))
+            exit(1)
 
         self.meta = {
             "student_id": "学号",
@@ -43,6 +49,7 @@ class Loader:
             "第一学位课程": "一学位",
             "第二学位课程": "二学位",
             "辅修专业课程": "辅修",
+            "交换生课程": "交换生课程", # 成绩为EX，绩点为N/A或空，不计入成绩计算
         }
         self.re_enter_mark = "重修"
 
@@ -82,12 +89,17 @@ class Loader:
                 course_grade_mark = data[self.meta["course_grade_mark"]]
                 semester = data[self.meta["semester"]]
                 course_class = self.course_class_meta[data[self.meta["course_class"]]]
-                course_type = self.course_type_meta[data[self.meta["course_type"]]]
+                if type(data[self.meta["course_type"]]) is str:
+                    course_type = self.course_type_meta[data[self.meta["course_type"]]]
+                elif math.isnan(data[self.meta["course_type"]]):
+                    course_type = self.course_type_meta["第一学位课程"]
+                else:
+                    logging.fatal(f"特殊课程标记 \"{data[self.meta['course_type']]}\"无法识别")
                 re_enter = data[self.meta["re-enter"]] == self.re_enter_mark
 
                 grade = Grade(student_id, course_number, course_grade, course_grade_mark, semester, course_class,
                               course_type, re_enter)
                 storage.students[student_id].course_index_list.append(len(storage.grades))
                 storage.grades.append(grade)
-        print("Totally {} students found.".format(len(storage.students)))
+        logging.info("Totally {} students found.".format(len(storage.students)))
         return storage
--- gpa/calculator/preprocess.py
+++ gpa/calculator/preprocess.py
@@ -1,33 +1,44 @@
 import pandas as pd
 import os
 import glob
+import logging
 
 class Preprocessor:
-    def __init__(self, path, output_path=None):
-        if os.path.isdir(path):
-            data_files = glob.glob(os.path.join(path, '*'))
-            self.data_files = [f for f in data_files if f[-4:] == ".xls"]
-        elif os.path.isfile(path) and path[-4:] == ".xls":
-            self.data_files = [path]
-        else:
-            raise TypeError("Path {} should be a valid xls file or directory.".format(path))
+    def __init__(self, paths, output_path=None):
+        self.data_files = []
+        self.output_paths = []
 
-        if output_path is None:
-            if os.path.isfile(path):
-                self.output_path = os.path.dirname(path)
+        if type(paths) is not list:
+            paths = [paths]
+
+        for path in paths:
+            if os.path.isdir(path):
+                data_files = glob.glob(os.path.join(path, '*'))
+                filtered_files = [f for f in data_files if f[-4:] == ".xls"]
+                self.data_files.extend(filtered_files)
+                self.output_paths.extend([path] * len(filtered_files))
+            elif os.path.isfile(path) and path[-4:] == ".xls":
+                self.data_files.extend([path])
+                self.output_paths.extend(os.path.dirname(path))
             else:
-                self.output_path = path
-        else:
-            self.output_path = output_path
+                logging.error("Path {} to be preprocessed should be a valid xls file or directory, xlsx file may not need to be preprocessed.".format(path))
+
+        if output_path is not None:
+            self.output_paths = [output_path] * len(self.data_files)
+
+        assert(len(self.output_paths) == len(self.data_files))
 
     def preprocess(self):
-        for fname in self.data_files:
+        outputs = []
+        for idx, fname in enumerate(self.data_files):
             df = pd.read_excel(fname)
             if df["特殊课程标记"].isnull().any():
-                df["特殊课程标记"] = df["特殊课程标记.1"]
-                df.drop(columns=["特殊课程标记.1"])
+                if "特殊课程标记.1" in df:
+                    df["特殊课程标记"] = df["特殊课程标记.1"]
+                    df.drop(columns=["特殊课程标记.1"])
             elif "特殊课程标记.1" in df and df["特殊课程标记.1"].isnull().any():
                 df.drop(columns=["特殊课程标记.1"])
-            output_fname = os.path.join(self.output_path, fname[:-4] + ".xlsx")
+            output_fname = os.path.join(self.output_paths[idx], fname[:-4] + ".xlsx")
             df.to_excel(output_fname, index=None)
-        return self.output_path
+            outputs.append(output_fname)
+        return outputs
